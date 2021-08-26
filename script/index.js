8e940b3c7f5fcc921e6921e0cb97b578cfc96f7a
wahyall <wahyall278@gmail.com>
2021-08-26 20:54:01 +0700

    pendemic-19

--- script/index.js
+++ script/index.js
@@ -1,10 +1,12 @@
 import navbar from "./js/navbar.js";
 import jumbotron from "./js/jumbotron.js";
 import vaksinasi from "./js/vaksinasi.js";
+import faq from "./js/faq.js";
 
 // Init
 (() => {
   navbar();
   jumbotron();
   vaksinasi();
+  faq();
 })();
---
+++ script/js/faq.js
@@ -0,0 +1,44 @@
+const faq = () => {
+  const accordionItem = [...document.querySelectorAll('.accordion .item')];
+  const accordionHeight = accordionItem.map(item => getComputedStyle(item.lastElementChild).height);
+  
+  accordionItem.forEach((item, i) => {
+    // Set ukuran height pada setiap accordion menjadi 0,
+    // tetapi set accordion pertama untuk terbuka terlebih dahulu (default)
+    i !== 0 ? closeAccordion(i) : openAccordion(i);
+
+    // Jalankan event click pada setiap accordion
+    item.firstElementChild.addEventListener('click', function () {
+      // Tutup semua accordion yang tidak diklik,
+      // agar nanti hanya accordion yang baru saja diklik yang terbuka
+      closeAllAccordionExcept(i);
+
+      // Toggle class open pada item yang diklik
+      item.classList.toggle('open');
+      
+      // Setelah itu cek apakah item tersebut memiliki class "open"
+      item.classList.contains('open') ? openAccordion(i) : closeAccordion(i);
+    })
+  });
+
+  function closeAccordion(i) {
+    accordionItem[i].classList.remove('open');
+    accordionItem[i].lastElementChild.style.height = 0;
+  }
+
+  function openAccordion(i) {
+    accordionItem[i].classList.add('open');
+    accordionItem[i].lastElementChild.style.height = accordionHeight[i];
+  }
+
+  function closeAllAccordionExcept(index) {
+    accordionItem.forEach((item, i) => {
+      if (i !== index) {
+        item.classList.remove('open');
+        item.lastElementChild.style.height = 0;
+      }
+    })
+  }
+}
+
+export default faq;
