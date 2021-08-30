816cde75e617f1935432310db5436b00499b1db5
wahyall <wahyall278@gmail.com>
2021-08-30 19:26:06 +0700

    pendemic-19

--- script/js/navbar.js
+++ script/js/navbar.js
@@ -1,7 +1,9 @@
 const navbar = () => {
+  const navbar = document.querySelector('.navbar');
   const navbarTogler = document.querySelector('.navbar .navbar-toggler');
   const navbarNav = document.querySelector('.navbar .navbar-nav');
 
+  // Toggle navbar on mobile
   navbarTogler.addEventListener('click', function () {
     navbarNav.classList.toggle('open');
   });
@@ -10,6 +12,15 @@ const navbar = () => {
       navbarNav.classList.remove('open');
     }
   });
+
+  // Change navbar style on scroll
+  window.onscroll = () => {
+    if (document.body.scrollTop > 64 || document.documentElement.scrollTop > 64) {
+      navbar.style.boxShadow = '0 0.75rem 0.75rem rgba(0, 11, 36, 0.1)';
+    } else {
+      navbar.style.boxShadow = 'unset';
+    }
+  }
 }
 
 export default navbar;
